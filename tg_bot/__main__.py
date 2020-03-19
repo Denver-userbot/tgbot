be0e5efa5d43581b0f1b6c16d67330ed837aa316
Denver <50638006+Denver-userbot@users.noreply.github.com>
2020-03-19 22:53:35 +0000

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,7 +19,7 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi {}, my name is {}! If you have any questions on how to use me, read /help - Channel Anti-Scam : @rr_help.
+Hi {}, my name is {}! If you have any questions on how to use me, read /help - Powered by @Denver02 .
 
 I also have a news channel, @antiscam_support for announcements on new features, downtime, etc.
 
