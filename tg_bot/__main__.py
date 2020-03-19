b2ef6d8b1c9ab5820d4f6353f559377e313ee511
Denver <50638006+Denver-userbot@users.noreply.github.com>
2020-03-19 22:40:41 +0000

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,9 +19,9 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi {}, my name is {}! If you have any questions on how to use me, read /help - and then head to @MarieSupport.
+Hi {}, my name is {}! If you have any questions on how to use me, read /help - Channel Anti-Scam : @rr_help.
 
-I also have a news channel, @MarieNews for announcements on new features, downtime, etc.
+I also have a news channel, @antiscam_support for announcements on new features, downtime, etc.
 
 You can find the list of available commands with /help.
 """
@@ -35,7 +35,6 @@ the things I can help you with.
  - /start: start the bot
  - /help: PM's you this message.
  - /help <module name>: PM's you info about that module.
- - /donate: information about how to donate!
  - /settings:
    - in PM: will send you your settings for all supported modules.
    - in a group: will redirect you to pm, with all that chat's settings.
